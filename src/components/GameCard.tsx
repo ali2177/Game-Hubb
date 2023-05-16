cf2d4a52a514f9e97321905c4b3d98bad7519fee
ali.2177 <ali.mehrjui@gmail.com>
2023-05-16 16:13:56 +0330

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,47 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
-import React from "react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import { Icon } from "@chakra-ui/react";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import {
+  FaWindows,
+  FaAndroid,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+} from "react-icons/fa";
+import { IconType } from "react-icons/lib";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+  const icons: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+  };
   return (
     <Card borderRadius="10px" overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack marginTop="10px" alignItems="center">
+          {game.parent_platforms.map((platform) => (
+            <Icon
+              color="gray.500"
+              fontSize="1.1rem"
+              as={icons[platform.platform.slug]}
+            />
+          ))}
+        </HStack>
       </CardBody>
     </Card>
   );
