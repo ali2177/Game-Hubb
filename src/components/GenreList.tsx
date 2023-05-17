123059995a1e8c4b4a7aebff957914b14c386336
ali.2177 <ali.mehrjui@gmail.com>
2023-05-17 17:58:21 +0330

    genres heading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
   ListItem,
   Button,
   transition,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -24,39 +25,47 @@ const GenreList = ({ onClick, genre }: Probs) => {
     onClick(genre);
   };
   return (
-    <List padding="5px">
-      {genres.map((genree) => (
-        <ListItem
-          _hover={{
-            background: "#3e495d",
-            color: "#fffe01",
-            borderRadius: "10px",
-            transition: "all 0.5s",
-          }}
-          paddingY="5px"
-          background={genree.id === genre?.id ? "#3e495d" : ""}
-          borderRadius={genree.id === genre?.id ? "10px" : ""}
-        >
-          <HStack>
-            <Image
-              borderRadius={8}
-              boxSize={genree.id === genre?.id ? "40px" : "32px"}
-              src={getCroppedImageUrl(genree.image_background)}
-            />
-            <Button
-              _hover={{ transition: "all 0.3s", fontWeight: "bold" }}
-              variant="unstyled"
-              color={genree.id === genre?.id ? "#fffe01" : ""}
-              fontWeight={genree.id === genre?.id ? "bold" : ""}
-              fontSize={genree.id === genre?.id ? "15px" : "13px"}
-              onClick={() => clickHandle(genree)}
-            >
-              {genree.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading padding="5px 10px" fontSize="2xl" marginBottom="1rem">
+        Genres
+      </Heading>
+      <List padding="0px 10px">
+        {genres.map((genree) => (
+          <ListItem
+            _hover={{
+              background: "#3e495d",
+              color: "#fffe01",
+              borderRadius: "10px",
+              transition: "all 0.5s",
+            }}
+            paddingY="5px"
+            background={genree.id === genre?.id ? "#3e495d" : ""}
+            borderRadius={genree.id === genre?.id ? "10px" : ""}
+          >
+            <HStack>
+              <Image
+                borderRadius={8}
+                boxSize={genree.id === genre?.id ? "40px" : "32px"}
+                objectFit="cover"
+                src={getCroppedImageUrl(genree.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                _hover={{ transition: "all 0.3s", fontWeight: "bold" }}
+                variant="unstyled"
+                color={genree.id === genre?.id ? "#fffe01" : ""}
+                fontWeight={genree.id === genre?.id ? "bold" : ""}
+                fontSize={genree.id === genre?.id ? "15px" : "13px"}
+                onClick={() => clickHandle(genree)}
+              >
+                {genree.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
